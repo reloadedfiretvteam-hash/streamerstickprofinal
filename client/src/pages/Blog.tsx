389884fa448b913e6c3b879495f8f5480f413d4f
reloadedfiretvteam <reloadedfiretvteam@gmail.com>
2026-01-21 19:11:22 -0500

    FIX: Add product schema to product-linked blog posts for Google rich results

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -19,6 +19,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import { SEOSchema } from "@/components/SEOSchema";
 
 interface BlogPost {
   id: string;
